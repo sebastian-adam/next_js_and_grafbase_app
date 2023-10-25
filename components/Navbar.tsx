6b21d1a9d6322681b4028cf13f7d73c14e5b0302
@sebastian.adam <sebastian.adam.work@gmail.com>
2023-10-24 19:56:01 -0500

    Enable user authentication

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,9 +2,10 @@ import { NavLinks } from "@/constants";
 import Image from "next/image";
 import Link from "next/link";
 import AuthProviders from "./AuthProviders";
+import { getCurrentUser } from "@/lib/session";
 
-const Navbar = () => {
-  const session = null;
+const Navbar = async () => {
+  const session = await getCurrentUser();
 
   return (
     <nav className="flexBetween navbar">
@@ -22,9 +23,17 @@ const Navbar = () => {
       </div>
 
       <div className="flexCenter gap-4">
-        {session ? (
+        {session?.user ? (
           <>
-            UserPhoto
+            {session?.user.image && (
+              <Image
+                src={session.user.image}
+                height={40}
+                width={40}
+                className="rounded-full"
+                alt={session.user.name}
+              />
+            )}
             <Link href="/create-project">Share work</Link>
           </>
         ) : (
